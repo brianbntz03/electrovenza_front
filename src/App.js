24c094de0a0a669e0f7040cca080cfad519283bc
Brian Benitez <brianbenitez0432@gmail.com>
2025-09-05 11:00:04 -0300

    agregado inicio sesion y cambios en el header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+// src/App.js
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header";
 import Aside from "./Components/Aside";
 import Content from "./Components/Content";
 import Footer from "./Components/Footer";
+import FormularioDeLogin from "./FormularioDeLogin";
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("jwt_token")
+  );
+  const navigate = useNavigate();
 
-  return(
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+    // Ya no es necesario guardar 'isLoggedIn'
+    navigate("/index");
+  };
+
+  const handleLogout = () => {
+    // Cuando el usuario cierra sesión, se elimina el token
+    localStorage.removeItem("jwt_token");
+    setIsAuthenticated(false);
+    navigate("/");
+  };
+
+  // Se ejecuta solo una vez para verificar la autenticación inicial
+  useEffect(() => {
+    setIsAuthenticated(!!localStorage.getItem("jwt_token"));
+  }, []);
+
+  return (
     <div>
-      <Header/>
-      <Router>  
-        <Content/>
-        <Aside/>
-      </Router>
-      <Footer/>
+      {isAuthenticated ? (
+        <>
+          <Header onLogout={handleLogout} />
+          <Aside />
+          <Content />
+          <Footer />
+        </>
+      ) : (
+        <FormularioDeLogin onLoginSuccess={handleLoginSuccess} />
+      )}
     </div>
-  )
+  );
 }
